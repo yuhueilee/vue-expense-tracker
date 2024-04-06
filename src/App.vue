fae3ed7faaa631a082440193f56884e5111f3971
yuhueilee <wendylee70127@gmail.com>
2024-04-06 11:34:07 +0800

    Create and import income expenses component

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <ExpenseHeader />
   <div class="container">
     <BalanceItem />
+    <IncomeExpenses />
   </div>
 </template>
 
 <script>
 import BalanceItem from './components/Balance.vue'
 import ExpenseHeader from './components/Header.vue'
+import IncomeExpenses from './components/IncomeExpenses.vue'
 
 export default {
   components: {
     ExpenseHeader,
-    BalanceItem
+    BalanceItem,
+    IncomeExpenses
   }
 }
 </script>
